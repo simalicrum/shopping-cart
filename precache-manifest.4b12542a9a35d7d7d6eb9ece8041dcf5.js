4386f0145c8f5c2e61392c1a1da2bc072b6a3b9f
Robert Reinert <robert.reinert@gmail.com>
2020-10-14 10:58:56 -0700

    Updates

---
+++ precache-manifest.4b12542a9a35d7d7d6eb9ece8041dcf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1e5cfb5c77d3734a56d094f982d299",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "2b04caac350725ad5e41",
+    "url": "/shopping-cart/static/css/main.8ee0f2ab.chunk.css"
+  },
+  {
+    "revision": "4bb736d1bd7d98c74b89",
+    "url": "/shopping-cart/static/js/2.f1ea1a01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.f1ea1a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b04caac350725ad5e41",
+    "url": "/shopping-cart/static/js/main.785b4a34.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
